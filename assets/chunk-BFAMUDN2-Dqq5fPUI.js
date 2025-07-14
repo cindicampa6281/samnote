99487203467221fd02528dfaa3913211e0df819e
nguyenvanchien1999prohehe <164542987+nguyenvanchien1999prohehe@users.noreply.github.com>
2025-07-14 15:34:44 +0700

    reyeryyre

---
+++ assets/chunk-BFAMUDN2-Dqq5fPUI.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./mermaid.core-xscpNy8Z.js";import{s as o}from"./isEmpty-DjndtHEa.js";var d=s((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
